111c803cfc65385504855df5b7e639ade03e809f
JasonCK <x13244231@gmail.com>
2021-12-31 01:15:36 +0800

    added my first parameter successful!

--- code.js
+++ code.js
@@ -1,28 +1,17 @@
-// TODO 可以限制只能輸入數字嗎？
-// TODO paremeter.on 要做檢查是否有物件被選擇、檢查 node type 是否為 FRAME
-figma.parameters.on('input', ({ query, key, result }) => {
+figma.parameters.on('input', ({ parameters, key, query, result }) => {
     switch (key) {
-        case 'layout-mode':
-            result.setSuggestions(["Horizontal", "Vertical"]);
+        case 'align-direction':
+            const directions = ['Vertical', 'Horizontal'];
+            result.setSuggestions(directions.filter(s => s.includes(query))); // filter() 是一個方法，還不太確定原理。
             break;
         default:
-            console.log("Default!");
             break;
     }
+    return;
 });
-function SetAutoLayout() {
-    const setLayer = figma.currentPage.selection[0];
-    // TODO 一定要用 if 設定 setLayer 的 type 是 FRAME 嗎？
-    if (setLayer.type === "FRAME") {
-        setLayer.layoutMode = "VERTICAL";
-        setLayer.paddingLeft = 24;
-        setLayer.paddingRight = 24;
-        setLayer.paddingTop = 16;
-        setLayer.paddingBottom = 16;
-        setLayer.itemSpacing = 12;
-        // TODO 這邊要做一個 else 顯示不是 FRAME 的 error
-        return;
-    }
+function setAutoLayout(parameters) {
+    var setTarget = figma.currentPage.selection;
+    console.log(setTarget);
+    // if(setTarget.type === "FRAME") {
+    // }
 }
-SetAutoLayout();
-figma.closePlugin("Done!");
--- code.ts
+++ code.ts
@@ -1,36 +1,23 @@
-// TODO 可以限制只能輸入數字嗎？
-// TODO paremeter.on 要做檢查是否有物件被選擇、檢查 node type 是否為 FRAME
-
-figma.parameters.on('input', ({query, key, result}: ParameterInputEvent) => {
+figma.parameters.on('input', ({ parameters, key, query, result }: ParameterInputEvent) => {
   switch (key) {
-    case 'layout-mode':
-      result.setSuggestions(["Horizontal", "Vertical"])
-      
+    case 'align-direction':
+      const directions = ['Vertical', 'Horizontal']
+      result.setSuggestions(directions.filter(s => s.includes(query))) // filter() 是一個方法，還不太確定原理。
       break;
   
     default:
-      console.log("Default!")
       break;
   }
+
+  return;
 })
 
-function SetAutoLayout() {
-  const setLayer = figma.currentPage.selection[0];
+function setAutoLayout(parameters) {
+  var setTarget = figma.currentPage.selection
+  console.log(setTarget)
 
-  // TODO 一定要用 if 設定 setLayer 的 type 是 FRAME 嗎？
-  if (setLayer.type === "FRAME") {
-    setLayer.layoutMode = "VERTICAL"
-    setLayer.paddingLeft = 24
-    setLayer.paddingRight = 24
-    setLayer.paddingTop = 16
-    setLayer.paddingBottom = 16
-    setLayer.itemSpacing = 12
+  // if(setTarget.type === "FRAME") {
 
-  // TODO 這邊要做一個 else 顯示不是 FRAME 的 error
-
-  return
-  }
+  // }
 }
 
-SetAutoLayout()
-figma.closePlugin("Done!")

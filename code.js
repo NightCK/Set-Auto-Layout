73ef3a072857ff62aaaf2a6856d2f73fb79782fe
JasonCK <x13244231@gmail.com>
2021-12-31 01:41:45 +0800

    stuck

--- code.js
+++ code.js
@@ -1,17 +1,22 @@
+var setTarget = figma.currentPage.selection[0]; // 記得要加 [0] 指定到陣列中的物件。
+function setAutoLayout(parameters) {
+    console.log(parameters);
+    // if(setTarget.type === "FRAME") {
+    //   setTarget.layoutMode = parameters
+    // }
+}
 figma.parameters.on('input', ({ parameters, key, query, result }) => {
     switch (key) {
         case 'align-direction':
-            const directions = ['Vertical', 'Horizontal'];
+            const directions = ["Vertical", "Horizontal"];
             result.setSuggestions(directions.filter(s => s.includes(query))); // filter() 是一個方法，還不太確定原理。
-            break;
-        default:
             break;
     }
     return;
 });
-function setAutoLayout(parameters) {
-    var setTarget = figma.currentPage.selection;
-    console.log(setTarget);
-    // if(setTarget.type === "FRAME") {
-    // }
-}
+figma.on('run', ({ command, parameters }) => {
+    switch (command) {
+        case "Vertical":
+        // setTarget.layoutMode = parameters // TODO
+    }
+});
